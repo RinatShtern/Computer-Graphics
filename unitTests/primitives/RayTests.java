64744f9cb4d89280d6ec384ceb7a4c9986925eeb
HilaHaik <hilashah@g.jct.ac.il>
2024-06-09 11:04:46 +0300

    p3 test

--- unitTests/primitives/RayTests.java
+++ unitTests/primitives/RayTests.java
@@ -10,15 +10,19 @@ class RayTests {
         // ============ Equivalence Partitions Tests ==============
         // TC01: point is in the middle of the ray
         Ray r1 = new Ray(new Point(1, 1, 1), new Vector(1, 1, 1));
-        assertEquals(new Point(1 + 1/Math.sqrt(3), 1 + 1/Math.sqrt(3), 1 + 1/Math.sqrt(3)), r1.getPoint(1), "Bad point");
+        double sqrt3 = Math.sqrt(3);
+        Point expected1 = new Point(1 + 1/sqrt3, 1 + 1/sqrt3, 1 + 1/sqrt3);
+        assertEquals(expected1, r1.getPoint(1), "Bad point");
 
         // TC02: point is not in the middle of the ray
-        Ray r2 = new Ray(new Point(1, 1, 1), new Vector(1, 1, 1));
-        assertEquals(new Point(1 + 2/Math.sqrt(3), 1 + 2/Math.sqrt(3), 1 + 2/Math.sqrt(3)), r2.getPoint(2), "Bad point");
+        Point expected2=new Point(1, 1, 1);
+        Ray r2 = new Ray(expected2, new Vector(1, 1, 1));
+        assertEquals(new Point(1 + 2/sqrt3, 1 + 2/sqrt3, 1 + 2/sqrt3), r2.getPoint(2), "Bad point");
 
         // =============== Boundary Values Tests ==================
         // TC03: point is at the beginning of the ray
+        Point expected3 =new Point(1, 1, 1);
         Ray r3 = new Ray(new Point(1, 1, 1), new Vector(1, 1, 1));
-        assertEquals(new Point(1, 1, 1), r3.getPoint(0), "Bad point");
+        assertEquals(expected3, r3.getPoint(0), "Bad point");
     }
 }
--- unitTests/primitives/VectorTests.java
+++ unitTests/primitives/VectorTests.java
@@ -124,7 +124,7 @@ public class VectorTests {
         // ============ Equivalence Partitions Tests ==============
         // TC01: Simple test
         assertEquals(1d, n.lengthSquared(), 0.00001, "wrong normalized vector length");
-        assertThrows(IllegalArgumentException.class, () -> v.crossProduct(n), "normalized vector is not in the same direction");
+        assertThrows(IllegalArgumentException.class, () -> v.crossProduct(n), "normalized vector is not in the same direction//");
         assertEquals(new Vector(0, 0.6, 0.8), n, "wrong normalized vector");
     }
 }

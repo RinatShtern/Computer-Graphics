0cbe07d48c5ee1e17f34814b3df2138a9125560d
Rinat Shteren <rinatshteren@gmail.com>
2024-06-17 14:20:08 +0300

    stage 5- add images and color

--- unitTests/renderer/ImageWriterTests.java
+++ unitTests/renderer/ImageWriterTests.java
@@ -1,4 +1,37 @@
+package renderer;
+
+import org.junit.jupiter.api.Test;
+import primitives.Color;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ImageWriterTests {
-  
+
+
+    /**
+     *  Produce a picture of gris line
+     */
+    @Test
+    void testWriteToImage() {
+        int nX = 800;
+        int nY = 500;
+
+        int interval = 50; // 800/50 = 16  500/50 = 10
+
+        Color yellowColor = new Color(255d,255d,0d);
+        Color redColor = new Color(255d,0,0d);
+
+        ImageWriter imageWriter = new ImageWriter("YellowSubmarine",nX,nY);
+        for (int i = 0; i < nX; i++) {
+            for (int j = 0; j < nY; j++) {
+                if(i % interval == 0 || j % interval == 0) {
+                    imageWriter.writePixel(i, j, redColor);
+                }
+                else {
+                    imageWriter.writePixel(i,j,yellowColor);
+                }
+            }
+        }
+        imageWriter.writeToImage();
+    }
 }
